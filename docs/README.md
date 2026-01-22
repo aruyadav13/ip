05900934f2771e3eced33f95ae39a53f630cedac
Aryan <aruyadav13@gmail.com>
2026-01-23 02:03:21 +0800

    Level-0: Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Tapu User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Tapu.java
@@ -0,0 +1,11 @@
+public class Tapu {
+    public static void main(String[] args) {
+
+        System.out.println("Hello I'm Tapu\n"
+                + "What can I do for you?\n"
+                + "________________________________\n"
+                + "Bye. Hope to see you again soon!" );
+
+
+    }
+}
