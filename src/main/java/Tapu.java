e8f99cad5618165387aac5d90ff98c0335fdde6b
Aryan <aruyadav13@gmail.com>
2026-01-27 00:21:59 +0800

    Level 1. Echo

--- src/main/java/Tapu.java
+++ src/main/java/Tapu.java
@@ -1,11 +1,27 @@
+import java.util.Scanner;
+
 public class Tapu {
     public static void main(String[] args) {
 
         System.out.println("Hello I'm Tapu\n"
                 + "What can I do for you?\n"
-                + "________________________________\n"
-                + "Bye. Hope to see you again soon!" );
+                + "________________________________\n");
 
+        String line;
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            line = in.nextLine();
 
+            if (line.equalsIgnoreCase("bye")) {
+                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                        + "Bye. Hope to see you again soon!\n"
+                        + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                break;
+            }
+
+            System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                    + line + "\n"
+                    + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+        }
     }
 }
