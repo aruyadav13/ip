fadaa7fa6d8318f97d68bb02aff15e11b028e67b
Aryan <aruyadav13@gmail.com>
2026-01-30 02:58:45 +0800

    Level 3. Mark as Done

--- src/main/java/Tapu.java
+++ src/main/java/Tapu.java
@@ -9,7 +9,7 @@ public class Tapu {
                 + "________________________________\n");
 
         String line;
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int taskCount = 0;
 
         Scanner in = new Scanner(System.in);
@@ -25,14 +25,51 @@ public class Tapu {
 
             else if (line.equalsIgnoreCase("list")) {
                 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + list[i]);
+                    System.out.println((i + 1) + ". " + list[i].toString());
                 }
                 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
             }
 
+            else if (line.startsWith("mark")) {
+                String[] parts = line.split(" ");
+                int taskIndex = Integer.parseInt(parts[1]) - 1;
+
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    list[taskIndex].markAsDone();
+
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                            + "Nice! I've marked this task as done:\n"
+                            + "  " + list[taskIndex].toString() + "\n"
+                            + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                } else {
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                            + "Error: Task " + parts[1] + " does not exist.\n"
+                            + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                }
+            }
+
+            else if (line.startsWith("unmark")) {
+                String[] parts = line.split(" ");
+                int taskIndex = Integer.parseInt(parts[1]) - 1;
+
+                if (taskIndex >= 0 && taskIndex < taskCount) {
+                    list[taskIndex].markAsNotDone();
+
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                            + "OK, I've marked this task as not done yet:\n"
+                            + "  " + list[taskIndex].toString() + "\n"
+                            + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                } else {
+                    System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
+                            + "Error: Task " + parts[1] + " does not exist.\n"
+                            + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+                }
+            }
+
             else {
-                list[taskCount] = line;
+                list[taskCount] = new Task(line);
                 taskCount++;
                 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
                         + "added: " + line + "\n"
