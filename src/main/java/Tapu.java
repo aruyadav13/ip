119493b5c272a516d54baf56a8d15383862345d4
Aryan <aruyadav13@gmail.com>
2026-01-30 15:46:16 +0800

    A-CodingStandard

--- src/main/java/Tapu.java
+++ src/main/java/Tapu.java
@@ -1,15 +1,13 @@
 import java.util.Scanner;
 
-
 public class Tapu {
     public static void main(String[] args) {
-
         System.out.println("Hello I'm Tapu\n"
                 + "What can I do for you?\n"
                 + "________________________________\n");
 
         String line;
-        Task[] list = new Task[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
 
         Scanner in = new Scanner(System.in);
@@ -21,55 +19,47 @@ public class Tapu {
                         + "Bye. Hope to see you again soon!\n"
                         + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
                 break;
-            }
-
-            else if (line.equalsIgnoreCase("list")) {
+            } else if (line.equalsIgnoreCase("list")) {
                 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + list[i].toString());
+                    System.out.println((i + 1) + ". " + tasks[i].toString());
                 }
                 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
-            }
-
-            else if (line.startsWith("mark")) {
+            } else if (line.startsWith("mark")) {
                 String[] parts = line.split(" ");
                 int taskIndex = Integer.parseInt(parts[1]) - 1;
 
                 if (taskIndex >= 0 && taskIndex < taskCount) {
-                    list[taskIndex].markAsDone();
+                    tasks[taskIndex].markAsDone();
 
                     System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
                             + "Nice! I've marked this task as done:\n"
-                            + "  " + list[taskIndex].toString() + "\n"
+                            + "  " + tasks[taskIndex].toString() + "\n"
                             + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
                 } else {
                     System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
                             + "Error: Task " + parts[1] + " does not exist.\n"
                             + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
                 }
-            }
-
-            else if (line.startsWith("unmark")) {
+            } else if (line.startsWith("unmark")) {
                 String[] parts = line.split(" ");
                 int taskIndex = Integer.parseInt(parts[1]) - 1;
 
                 if (taskIndex >= 0 && taskIndex < taskCount) {
-                    list[taskIndex].markAsNotDone();
+                    tasks[taskIndex].markAsNotDone();
 
                     System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
                             + "OK, I've marked this task as not done yet:\n"
-                            + "  " + list[taskIndex].toString() + "\n"
+                            + "  " + tasks[taskIndex].toString() + "\n"
                             + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
                 } else {
                     System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
                             + "Error: Task " + parts[1] + " does not exist.\n"
                             + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
                 }
-            }
-
-            else {
-                list[taskCount] = new Task(line);
+            } else {
+                tasks[taskCount] = new Task(line);
                 taskCount++;
                 System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n"
                         + "added: " + line + "\n"
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,19 +2,36 @@ public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Initializes a new Task with the specified description.
+     * The task is initially marked as not done.
+     *
+     * @param description The description of the task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Returns the status icon of the task.
+     *
+     * @return "X" if the task is done, otherwise a space.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /**
+     * Marks the task as not done.
+     */
     public void markAsNotDone() {
         this.isDone = false;
     }
