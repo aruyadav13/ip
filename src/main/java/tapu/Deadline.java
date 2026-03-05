1eb6e3aedd0603f832d2998b7a8ec04ed4f975a3
Aryan <aruyadav13@gmail.com>
2026-03-06 03:14:09 +0800

    Add LocalDate parsing for deadlines and events

--- src/main/java/tapu/Deadline.java
+++ src/main/java/tapu/Deadline.java
@@ -1,15 +1,29 @@
 package tapu;
 
-public class Deadline extends Task{
-    protected String by;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class Deadline extends Task {
+    protected String byString;
+    protected LocalDate byDate;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        try {
+            // Try to parse the input as a Date (yyyy-mm-dd)
+            this.byDate = LocalDate.parse(by);
+            // If successful, format it to "MMM d yyyy" (e.g., Oct 15 2019)
+            this.byString = byDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } catch (DateTimeParseException e) {
+            // If it's not a valid date format (e.g., "Sunday"), just save the string
+            this.byString = by;
+            this.byDate = null;
+        }
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "[D]" + super.toString() + " (by: " + byString + ")";
     }
 }
--- src/main/java/tapu/Event.java
+++ src/main/java/tapu/Event.java
@@ -1,18 +1,32 @@
 package tapu;
 
-public class Event extends Task{
-    protected String from;
-    protected String to;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class Event extends Task {
+    protected String fromString;
+    protected String toString;
+    protected LocalDate fromDate;
+    protected LocalDate toDate;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.fromString = parseDate(from);
+        this.toString = parseDate(to);
+    }
+
+    private String parseDate(String dateStr) {
+        try {
+            LocalDate date = LocalDate.parse(dateStr);
+            return date.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } catch (DateTimeParseException e) {
+            return dateStr;
+        }
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + from + " to: " + to +")";
+        return "[E]" + super.toString() + " (from: " + fromString + " to: " + toString + ")";
     }
 }
-
